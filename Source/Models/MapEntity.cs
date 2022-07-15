d12d41d70ed81aa0c731b431a7a5c6690202862b
tapsaman <tapiovalkama93@gmail.com>
2022-07-15 04:23:58 +0300

    project file reorganizing

--- Source/Models/MapEntity.cs
+++ Source/Models/MapEntity.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using MonoGameTestGame.Controls;
-using MonoGameTestGame.Managers;
-using MonoGameTestGame.Models;
 using MonoGameTestGame.Sprites;
 
 namespace MonoGameTestGame
